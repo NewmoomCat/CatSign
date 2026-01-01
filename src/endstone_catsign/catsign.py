29de3662c97c7fc7bd961d415c45a5a401c9607b
NewmoonNeko <3649745663@qq.com>
2026-01-02 07:08:38 +0800

    add some fix

--- src/endstone_catsign/catsign.py
+++ src/endstone_catsign/catsign.py
@@ -1,5 +1,7 @@
+import datetime
 import json
 import os
+from pathlib import Path
 import time
 
 from endstone import *
@@ -7,69 +9,80 @@ from endstone.command import *
 from endstone.plugin import Plugin
 
 data_dir = os.path.join(os.getcwd(), "plugins", "CatSign")
-config_data_file_path = os.path.join(data_dir, "config.json")
+
 
 class CatSign(Plugin):
     api_version = "0.10"
     authors = ["NewmoonNeko"]
 
     commands = {
-        "cs": {
+        "qd": {
             "description": "签到指令",
-            "usage": ["/cs"],
-            "permissions": ["cat.cmd.cs"]
+            "usage": ["/qd"],
+            "permissions": ["cat.cmd.qd"]
         }
     }
 
     permissions = {
-        "cat.cmd.cs": {
+        "cat.cmd.qd": {
             "description": "签到指令",
             "default": True
         }
     }
 
-    def __init__(self):
-        self.conf_data = {
-            "money": 50
-        }
-        self.money = self.conf_data["money"]
+
+
 
     def on_load(self):
         self.logger.info(f"{ColorFormat.AQUA}签到插件加载中...")
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-        if not os.path.exists(config_data_file_path):
-            self.logger.info(f"{ColorFormat.RED}未发现配置文件")
-            self.logger.info(f"{ColorFormat.YELLOW}创建中...")
-            with open(config_data_file_path, "w", encoding="utf-8") as f:
-                json_str = json.dumps(self.conf_data, indent=4, ensure_ascii=False)
-                f.write(json_str)
-            self.conf_data = json.loads(json_str)
-            self.logger.info(f"{ColorFormat.GREEN}文件创建成功")
-
 
     def on_enable(self):
-        with open(config_data_file_path, "r", encoding="utf-8") as f:
-            self.conf_data = json.loads(f.read())
+        self.js = self.server.plugin_manager.get_plugin("ye111566_jsonmoney")
         self.um = self.server.plugin_manager.get_plugin("umoney")
+
         if self.server.plugin_manager.get_plugin("umoney"):
             self.logger.info(f"{ColorFormat.GREEN}检测到UMoney经济插件")
         else:
             self.logger.info(f"{ColorFormat.RED}未检测到UMoney, 插件将卸载")
             self.server.plugin_manager.disable_plugin(self)
+
+        #if you money core is jsonmoney
+        """ 
+        if self.server.plugin_manager.get_plugin("ye111566_jsonmoney"):
+            self.logger.info(f"{ColorFormat.GREEN}检测到UMoney经济插件")
+        else:
+            self.logger.info(f"{ColorFormat.RED}未检测到UMoney, 插件将卸载")
+            self.server.plugin_manager.disable_plugin(self)
+        """
+
         self.logger.info(f"{ColorFormat.AQUA}CatSign签到插件,\n{ColorFormat.AQUA}作者: NewmoonNeko")
 
     def on_command(self, sender: CommandSender, cmd: Command, args: list[str]):
-        m = self.money
+        m = 50 #默认50块
+        qz = "签到插件" #插件前缀
         u = sender.name
-        if cmd.name == "cs":
-            if isinstance(sender, Player):
-                self.um.api_get_money_data()
-                self.um.api_change_player_money(u, m)
-                sender.send_message(f"{ColorFormat.GREEN}签到成功, 获得{m}块钱!")
-            elif isinstance(sender, Player):
-                sender.send_message(f"{ColorFormat.YELLOW}你今天签到过了, 不要重复签到")
-            else:
-                sender.send_message(f"{ColorFormat.YELLOW}不要在控制台使用此命令哟!")
+        ny = datetime.datetime.now().year
+        nm = datetime.datetime.now().month
+        nd = datetime.datetime.now().day
+        tm_data = os.path.join(data_dir, f"{ny}-{nm}-{nd}")
+        tm_data_json = f"{sender.name}.json"
+        if isinstance(sender, Player):
+            match cmd.name:
+                case "qd":
+                    if not os.path.exists(tm_data):
+                        os.mkdir(tm_data)
+                        with open(os.path.join(tm_data, tm_data_json), "w") as f:
+                            f.write('{"time"}')
+                        self.um.api_change_player_money(u, m)
+                        sender.send_message(f"[{qz}]签到成功,你获得了{m}块钱")
+                    elif not os.path.exists(tm_data, ):
+                        with open(os.path.join(tm_data, tm_data_json), "w") as f:
+                            f.write('{"time"}')
+                        self.um.api_change_player_money(u, m)
+                        sender.send_message(f"[{qz}]签到成功,你获得了{m}块钱")
+                    elif os.path.exists(os.path.join(tm_data, tm_data_json)):
+                        sender.send_message(f"[{qz}]你今天已经签到过了哟~")
             return True
-        return False
+        else:
+            sender.send_message(f"[{qz}]不要在控制台使用此命令哟")
+            return True
